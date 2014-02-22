fad67f6b66f88055c778a318636cb34988859caf
aoneilsmith <a.oneilsmith@umiami.edu>
2014-02-22 13:59:31 -0500

    Initial Commit

---
+++ MuHack/MuHackAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MuHackAppDelegate.h
+//  MuHack
+//
+//  Created by Andrew O'Neil-Smith on 2/22/14.
+//  Copyright (c) 2014 MuHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MuHackAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MuHack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MuHack
+//
+//  Created by Andrew O'Neil-Smith on 2/22/14.
+//  Copyright (c) 2014 MuHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MuHackAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MuHackAppDelegate class]));
+    }
+}
